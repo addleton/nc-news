ebc4fef34c0440174849291fa6390c9cb3dc3e9b
addleton <michaeladdleton1@gmail.com>
2023-12-11 14:28:36 +0000

    fixes merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Articles from "./components/Articles";
 import SingleArticle from "./components/SingleArticle";
 import { useState } from "react";
 
 function App() {
   const [articles, setArticles] = useState([]);
   return (
-    <Routes>
-      <Route path="/articles/:articleid" element={<SingleArticle articles={articles} setArticles={setArticles} />} />
-    </Routes>
+    <>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Articles />} />
+	<Route path="/articles/:articleid" element={<SingleArticle articles={articles} setArticles={setArticles
+      </Routes>
+    </>
   );
 }
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const api = axios.create({ baseURL: "https://nc-news-xe7y.onrender.com/api" });
 
+export const getAllArticles = () => {
+  return api.get("/articles");
+};
+
 export const getArticleById = (articleId) => {
   return api.get(`/articles/${articleId}`);
 };
