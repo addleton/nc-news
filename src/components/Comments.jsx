f7411cbfa4b84cd0b9973f1a6e64de104c867c25
addleton <michaeladdleton1@gmail.com>
2023-12-12 14:05:48 +0000

    adds loading message to comment section

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -3,16 +3,22 @@ import { getCommentsByArticle } from "../utils/api";
 import SingleComment from "./SingleComment";
 
 const Comments = ({ articleId }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [comments, setComments] = useState([]);
   useEffect(() => {
     getCommentsByArticle(articleId).then(({ data: { comments } }) => {
       setComments(comments);
+      setIsLoading(false);
     });
   }, []);
 
+  if (isLoading) {
+    return <p>fetching comments...</p>;
+  }
+
   return (
     <>
-      <ul id='comment-section'>
+      <ul id="comment-section">
         {comments.map((comment) => {
           return <SingleComment comment={comment} key={comment.comment_id} />;
         })}
